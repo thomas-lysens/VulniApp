edc6ab0914310740821abfb27efd0ca2ec0de6db
Thomas Lysens <lyssec@DESKTOP-EDQB01B.localdomain>
2022-09-14 05:41:36 +0200

    Added first setup. Imported header.php and created a basic hero section. To be continued...

---
+++ pages/home.php
@@ -0,0 +1,19 @@
+<head>
+    <title>VulniApp - Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="./assets/css/style.css" type="text/css" rel="stylesheet"/>
+</head>
+<body>
+    <?php include_once('./components/header.php')?>
+    <main>
+        <div class="px-4 py-5 my-5 text-center">
+            <h1 class="display-5 fw-bold">VulniApp</h1>
+            <div class="col-lg-6 mx-auto">
+                <p class="lead mb-4"></p>
+                <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                    <button type="button" class="btn btn-primary">Upload Media</button>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
