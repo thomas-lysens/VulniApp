1a7c189a4bf7c76fecb286664df7c87402f6950a
Thomas Lysens <lyssec@DESKTOP-EDQB01B.localdomain>
2022-09-14 22:13:04 +0200

    Added footer component with styling and lay-out

---
+++ components/footer.php
@@ -0,0 +1,23 @@
+<footer>
+    <div class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top container">
+        <p class="col-md-4 mb-0 text-muted">&copy; <?php $year = date("Y"); echo $year; ?> VulniApp</p>
+        <a class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none" href="./">VulniApp</a>
+        <ul class="nav col-md-4 justify-content-end">
+            <li class="nav-item">
+                <a href="/" class="nav-link px-2 text-muted">Home</a>
+            </li>
+            <li class="nav-item">
+                <a href="/about" class="nav-link px-2 text-muted">About</a>
+            </li>
+            <li class="nav-item">
+                <a href="/upload" class="nav-link px-2 text-muted">Upload</a>
+            </li>
+            <li class="nav-item">
+                <a href="/contact" class="nav-link px-2 text-muted">Contact</a>
+            </li>
+            <li class="nav-item">
+                <a href="/login" class="nav-link px-2 text-muted">Login</a>
+            </li>
+        </ul>
+    </div>
+</footer>
