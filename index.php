f4c0667a36a715245078abf9173641868c884675
Thomas Lysens <lyssec@DESKTOP-EDQB01B.localdomain>
2022-09-14 05:40:14 +0200

    Added basic routing based on path, rendering from /pages/ the file linked to pathname

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-<?php
+<!DOCTYPE html>
+<html>
+<?
+$path = $_SERVER['REQUEST_URI'];
+
+switch ($path) {
+    case '/':
+        require_once(__DIR__ . '/pages/home.php');
+        break;
+    case '/about':
+        require_once(__DIR__ . '/pages/about.php');
+        break;
+    case '/upload':
+        require_once(__DIR__ . '/pages/upload.php');
+        break;
+    case '/contact':
+        require_once(__DIR__ . '/pages/contact.php');
+        break;
+    case '/login':
+        require_once(__DIR__ . '/pages/login.php');
+        break;
+    default:
+        echo "<h1>Something went wrong!</h1>";
+}
+
+?>
+</html>
