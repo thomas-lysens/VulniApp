4f8f903ae07c2bda932c9f2eaef9fd3a7b5f1cbb
Thomas Lysens <lyssec@DESKTOP-EDQB01B.localdomain>
2022-09-14 06:05:46 +0200

    Expanded the hero section further

--- pages/home.php
+++ pages/home.php
@@ -6,10 +6,10 @@
 <body>
     <?php include_once('./components/header.php')?>
     <main>
-        <div class="px-4 py-5 my-5 text-center">
+        <div class="px-4 py-5 my-5 text-center bg-white hero">
             <h1 class="display-5 fw-bold">VulniApp</h1>
             <div class="col-lg-6 mx-auto">
-                <p class="lead mb-4"></p>
+                <p class="lead mb-4">Welcome to our cloud storage application! If you need to save some files and/or media, but you don't have the space on your computer... upload it to our cloud! We'll keep it safe for you!</p>
                 <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
                     <button type="button" class="btn btn-primary">Upload Media</button>
                 </div>
