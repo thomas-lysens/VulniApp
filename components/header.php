49538c99f77964c5384ab29c09ed2e0fd7946f6a
Thomas Lysens <lyssec@DESKTOP-EDQB01B.localdomain>
2022-09-14 05:43:18 +0200

    Added header.php component with the layout for the header.

--- components/header.php
+++ components/header.php
@@ -1,1 +1,21 @@
-<?php
+<header>
+    <div class="navbar navbar-dark bg-primary">
+        <div class="container">
+            <div class="logo-section col">
+                <a class="navbar-brand" href="./">VulniApp</a>
+            </div>
+            <nav class="col">
+                <ul class="nav" style="float: right;">
+                    <li class="nav-item"><a href="/" class="nav-link active" aria-current="page">Home</a></li>
+                    <li class="nav-item"><a href="/about" class="nav-link" style="color: white;">About</a></li>
+                    <li class="nav-item"><a href="/upload" class="nav-link" style="color: white;">Upload</a></li>
+                    <li class="nav-item"><a href="/contact" class="nav-link" style="color: white;">Contact</a></li>
+                </ul>
+            </nav>
+            <div class="login-section col" style="text-align: right;">
+                <button type="button" class="btn btn-outline-light">Login</button>
+                <button type="button" class="btn btn-outline-light">Register</button>
+            </div>
+        </div>
+    </div>
+</header>
