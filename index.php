189594f8d8cf9b287c800d6cb9f09e1e2d40bbb9
Thomas Lysens <lyssec@DESKTOP-EDQB01B.localdomain>
2022-09-15 23:33:38 +0200

    Added extra routes

--- index.php
+++ index.php
@@ -13,14 +13,20 @@ switch ($path) {
     case '/upload':
         require_once(__DIR__ . '/pages/upload.php');
         break;
+    case '/pricing':
+        require_once(__DIR__ . '/pages/pricing.php');
+        break;
     case '/contact':
         require_once(__DIR__ . '/pages/contact.php');
         break;
     case '/login':
         require_once(__DIR__ . '/pages/login.php');
         break;
+    case '/register':
+        require_once(__DIR__ . '/pages/register.php');
+        break;
     default:
-        echo "<h1>Something went wrong!</h1>";
+        require_once(__DIR__ . '/pages/404.php');
 }
 
 ?>
