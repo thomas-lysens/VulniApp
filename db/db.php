2f156531dbb7bceae0524a7af48531cd0a190e39
Thomas Lysens <lyssec@DESKTOP-EDQB01B.localdomain>
2022-09-15 23:34:14 +0200

    Added a DB class for connection to MySQL server

--- db/db.php
+++ db/db.php
@@ -1,1 +1,14 @@
 <?php
+class DB {
+    public function __construct() {}
+
+    public function connect($server, $username, $password, $database, $port) {
+        $conn = mysqli_connect($server, $username, $password, $database, $port);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->error);
+        } else {
+            return $conn;
+        }
+    }
+}
+?>
