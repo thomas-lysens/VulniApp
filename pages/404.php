30681a1ae1aff07aad9e5dcd209c331723cfaf47
Thomas Lysens <lyssec@DESKTOP-EDQB01B.localdomain>
2022-09-15 23:30:53 +0200

    Added a 404 and a pricing page for fill up

---
+++ pages/404.php
@@ -0,0 +1,14 @@
+<head>
+    <title>VulniApp - Not found</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="./assets/css/style.css" type="text/css" rel="stylesheet"/>
+</head>
+<body>
+<?php include_once('./components/header.php')?>
+<main>
+    <div class="container hero">
+        <h1>404 - Page not found</h1>
+    </div>
+</main>
+<?php include_once('./components/footer.php')?>
+</body>
---
+++ pages/pricing.php
@@ -0,0 +1,11 @@
+<head>
+    <title>VulniApp - Pricing</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="./assets/css/style.css" type="text/css" rel="stylesheet"/>
+</head>
+<body>
+<?php include_once('./components/header.php')?>
+<main>
+</main>
+<?php include_once('./components/footer.php')?>
+</body>
