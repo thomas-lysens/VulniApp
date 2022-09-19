0eda6e3dcec63cac5a9b68b31ac7acdea3f6fc84
Thomas Lysens <thomas@lyssec.com>
2022-09-19 22:24:38 +0200

    Changed from mysqli to PDO connection

--- db/db.php
+++ db/db.php
@@ -3,12 +3,8 @@ class DB {
     public function __construct() {}
 
     public function connect($server, $username, $password, $database, $port) {
-        $conn = mysqli_connect($server, $username, $password, $database, $port);
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->error);
-        } else {
-            return $conn;
-        }
+        $conn = new PDO("mysql:host=" . $server . ";port=" . $port . ";dbname=" . $database . ";", $username, $password);
+	return $conn;
     }
 }
 ?>
