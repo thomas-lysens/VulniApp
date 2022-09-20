feb364be6631580cb39c313569d927a700f16f02
Thomas Lysens <thomas@lyssec.com>
2022-09-20 10:13:44 +0200

    Add conditional loading based on if user is logged in or not

--- components/header.php
+++ components/header.php
@@ -4,6 +4,18 @@
             <div class="logo-section col">
                 <a class="navbar-brand" href="./">VulniApp</a>
             </div>
+	    <?php if ($user_logged_in) { ?>
+	    <nav class="col">
+                <ul class="nav justify-content-center">
+                    <li class="nav-item"><a href="/" class="nav-link" style="color: white;">Dashboard</a></li>
+                    <li class="nav-item"><a href="/upload" class="nav-link" style="color: white;">Upload</a></li>
+                    <li class="nav-item"><a href="/documents" class="nav-link" style="color: white;">Documents</a></li>
+                </ul>
+            </nav>
+            <div class="login-section col" style="text-align: right;">
+                <button type="button" class="btn btn-outline-light" onclick="window.location.pathname = '/logout';">Logout</button>
+            </div>
+	    <?php } else { ?>
             <nav class="col">
                 <ul class="nav justify-content-center">
                     <li class="nav-item"><a href="/" class="nav-link" style="color: white;">Home</a></li>
@@ -16,6 +28,7 @@
                 <button type="button" class="btn btn-outline-light" onclick="window.location.pathname = '/login';">Login</button>
                 <button type="button" class="btn btn-outline-light" onclick="window.location.pathname = '/register';">Register</button>
             </div>
+	    <?php } ?>
         </div>
     </div>
 </header>
